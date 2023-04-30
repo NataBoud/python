ee8887e68fb1a94421fd387b8d2525743d08528f
NataBoud <natalia.boudard@gmail.com>
2023-04-30 11:36:14 +0200

    one lesson_Variable
    Create variables

--- python.py
+++ python.py
@@ -1,1 +1,31 @@
-#
+#  - comments in python
+print("Hello world")
+# ** - возведение в степень
+# % - деление по модулю (записывается остаток)
+# // - целочисленное деление с отбрасыванием дробной части
+# / - деление, * - умножение, - + вычитание и сложение
+print(23 % 7)
+print(23 // 7)
+# целые числа вещественные числа и строки
+# int() - целочисленный тип данных, целые числа
+# "" - str() тип данных, строки
+# float - вещественные числа либо числа с плавающей точкой
+# my_name - snake_case
+my_name = "Natalia "
+print(my_name)
+# конкотинация Concatenation operator
+my_monmari = "Quentin"
+my_number = 13
+print(my_name + " " + my_monmari)
+print(my_name + "13")
+print(my_name + " " + str(my_number))
+# функция ф стринг позволяет вкладывать все интерполировать
+print(f"{my_name} nombre prefere {my_number}")
+print(my_name * 5)
+# variable - это область памяти компьютера, в которой может храниться еденичное значение, знак = оперетор присваивания  и сохраняет значение
+color = "rouge"
+print(color)
+
+
+
+
