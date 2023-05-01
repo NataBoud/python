48102c1a3523935e095d0329e7cf3a67b7c6e054
NataBoud <natalia.boudard@gmail.com>
2023-05-01 23:51:01 +0200

    home_work

---
+++ home_work.py
@@ -0,0 +1,8 @@
+#Блок схема: if name = natalia print natalia
+# elif number == 13 print...le nombre est deviné
+# else le nombre n'est pas deviné
+
+# если меньше 18 доступ запрещен
+# если возраст равен 18 мы подумаем
+# если возраст больше 18 мы вход разрещен
+# переменная h=18
